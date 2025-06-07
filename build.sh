626c2ac697dc8a2688db5a126770ee3dad9d288d
andremunay <123027715+andremunay@users.noreply.github.com>
2025-06-07 08:13:25 -0600

    chore(monitoring): add Prometheus metrics with Micrometer registry

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-./mvnw spotless:apply
-./mvnw clean verify 2>&1 | tee build.log
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Clear or create log file
-: > out.log
-
-# Run Maven, log to file, pipe JSON lines to jq
-./mvnw -q clean spring-boot:run 2>&1 | tee out.log | jq .
